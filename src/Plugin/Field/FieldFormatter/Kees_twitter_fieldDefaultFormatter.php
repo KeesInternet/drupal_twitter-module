1f1d8cc6be3e6559ae100ecd3698dc236172cb6a
ZEF <zef@kees-tm.nl>
2019-07-18 13:27:34 +0200

    Fix bug: hide error

--- src/Plugin/Field/FieldFormatter/Kees_twitter_fieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/Kees_twitter_fieldDefaultFormatter.php
@@ -22,44 +22,45 @@ class Kees_twitter_fieldDefaultFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
-    $config = \Drupal::config('kees_twitter_field.twitter_api');
-    $settings = [];
-    $settings['oauth_access_token'] = $config->get('oauth_access_token');
-    $settings['oauth_access_token_secret'] = $config->get('oauth_access_token_secret');
-    $settings['consumer_key'] = $config->get('consumer_key');
-    $settings['consumer_secret'] = $config->get('consumer_secret');
+    public function viewElements(FieldItemListInterface $items, $langcode) {
+        $config = \Drupal::config('kees_twitter_field.twitter_api');
+        $settings = [];
+        $settings['oauth_access_token'] = $config->get('oauth_access_token');
+        $settings['oauth_access_token_secret'] = $config->get('oauth_access_token_secret');
+        $settings['consumer_key'] = $config->get('consumer_key');
+        $settings['consumer_secret'] = $config->get('consumer_secret');
 
-    $output = [];
-    $build["#theme"] = "kees_twitter_field_formatter";
+        $output = [];
+        $build["#theme"] = "kees_twitter_field_formatter";
 
-    foreach ($items as $delta => $item) {
-      $filter = $item->value;
-      $count = $item->count;
-      if (isset($filter) && $filter != "") {
-        $build["#filter"] = $filter;
-        $twitter = new TwitterOAuth($settings['consumer_key'], $settings['consumer_secret'], $settings['oauth_access_token'], $settings['oauth_access_token_secret']);
-        if ($filter[0] == "@") {
-          $build["#type"] = "username";
-          $tweets = $twitter->get("statuses/user_timeline", ["screen_name" => $filter, "count" => $count, "lang" => "nl"]);
-          foreach ($tweets as $key => $tweet) {
-            $build["#tweets"][] = $tweet;
-          }
-          $output[$delta] = $build;
+        foreach ($items as $delta => $item) {
+        $filter = $item->value;
+        $count = $item->count;
+        if (isset($filter) && $filter != "") {
+            $build["#filter"] = $filter;
+            $twitter = new TwitterOAuth($settings['consumer_key'], $settings['consumer_secret'], $settings['oauth_access_token'], $settings['oauth_access_token_secret']);
+            if ($filter[0] == "@") {
+                $build["#type"] = "username";
+                $tweets = $twitter->get("statuses/user_timeline", ["screen_name" => $filter, "count" => $count, "lang" => "nl"]);
+                foreach ($tweets as $key => $tweet) {
+                    $build["#tweets"][] = $tweet;
+                }
+                $output[$delta] = $build;
+            } else {
+                $tweets = $twitter->get("search/tweets", ["q" => $filter, "count" => $count, "lang" => "nl"]);
+                if(isset($tweets->statuses)){
+                    foreach ($tweets->statuses as $key => $tweet) {
+                        $build["#tweets"][] = $tweet;
+                    }
+                    $build["#type"] = "hashtag";
+                    $output[$delta] = $build;
+                }
+            }
         }
-        else {
-          $tweets = $twitter->get("search/tweets", ["q" => $filter, "count" => $count, "lang" => "nl"]);
-          foreach ($tweets->statuses as $key => $tweet) {
-            $build["#tweets"][] = $tweet;
-          }
-          $build["#type"] = "hashtag";
-          $output[$delta] = $build;
+
         }
-      }
+        return $output;
 
     }
-    return $output;
-
-  }
 
 }
